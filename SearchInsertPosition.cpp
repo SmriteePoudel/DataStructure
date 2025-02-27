83c1ce66d26eb73fcf328be43b721ad98326560f
Smritee <esmritypoudel@gmail.com>
2025-02-27 14:27:48 +0530

    SearchInsertPosition.cpp

---
+++ SearchInsertPosition.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+class Solution {
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        // Binary search
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Target is in the right half
+            } else {
+                right = mid - 1; // Target is in the left half
+            }
+        }
+        
+        // If we reach here, target was not found
+        // 'left' now points to where the target should be inserted
+        return left;
+    }
+};
